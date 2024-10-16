a3b5ef8542872fad1aac096ca30d02ca3f536622
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-10-15 22:21:46 -0600

    Loops, Ifs, When and Try Catch II

---
+++ When/src/javacode/SwitchExample.java
@@ -0,0 +1,21 @@
+package javacode;
+
+public class SwitchExample {
+
+    public static void main(String[] args) {
+        int num = 200;
+        switch (num) {
+            case 100:
+                System.out.println("100");
+                break;
+            case 200:
+                System.out.println("200");
+                break;
+            case 300:
+                System.out.println("300");
+                break;
+            default:
+                System.out.println("Did not match anything");
+        }
+    }
+}
